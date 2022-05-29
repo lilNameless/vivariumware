25abc88031c7fad958f07a298f0c10c599bd1586
lilNameless <matgamer543@gmail.com>
2022-05-29 12:27:23 +0200

    vivariumware

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    element.style.transform = 'rotate(90deg)';
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    element.style.transform = 'rotate(-90deg)';
   }
